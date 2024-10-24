e753f1a9cbbc2d6971f1014925239559d03d8d04
suresh-sterna <sureshs@sternadevices.in>
2024-10-24 15:11:11 +0530

    added more colliders

--- main.go
+++ main.go
@@ -19,63 +19,62 @@ type Sprite struct {
 type Villain struct {
 	*Sprite
 }
+
 type Game struct {
 	Hero      *Sprite
-	villain   []*Villain
+	villains  []*Villain
 	caught    bool
 	colliders []image.Rectangle
-
-	// Paused  bool
 }
 
-func CheckCollisionHorizontal(sprite *Sprite, coliders []image.Rectangle) {
-	for _, col := range coliders {
+func CheckCollisionHorizontal(sprite *Sprite, colliders []image.Rectangle) {
+	for _, col := range colliders {
 		if col.Overlaps(
 			image.Rect(
 				int(sprite.X),
 				int(sprite.Y),
-				int(sprite.X)+16.0,
-				int(sprite.Y)+16.0),
+				int(sprite.X)+16,
+				int(sprite.Y)+16,
+			),
 		) {
-			if sprite.Dx > 0.0 {
-				sprite.X = float64(col.Min.X) - 16.0
-			} else if sprite.Dx < 0.0 {
+			if sprite.Dx > 0 {
+				sprite.X = float64(col.Min.X) - 16
+			} else if sprite.Dx < 0 {
 				sprite.X = float64(col.Max.X)
 			}
 		}
 	}
 }
 
-func CheckCollisionVertical(sprite *Sprite, coliders []image.Rectangle) {
-	for _, col := range coliders {
+func CheckCollisionVertical(sprite *Sprite, colliders []image.Rectangle) {
+	for _, col := range colliders {
 		if col.Overlaps(
 			image.Rect(
 				int(sprite.X),
 				int(sprite.Y),
-				int(sprite.X)+16.0,
-				int(sprite.Y)+16.0),
+				int(sprite.X)+16,
+				int(sprite.Y)+16,
+			),
 		) {
-			if sprite.Dy > 0.0 {
-				sprite.Y = float64(col.Min.Y) - 16.0
-			} else if sprite.Dy < 0.0 {
+			if sprite.Dy > 0 {
+				sprite.Y = float64(col.Min.Y) - 16
+			} else if sprite.Dy < 0 {
 				sprite.Y = float64(col.Max.Y)
 			}
 		}
 	}
 }
 
+func (g *Game) Border() {
+	g.Hero.X = math.Max(g.Hero.X, 0.0)
+	g.Hero.Y = math.Max(g.Hero.Y, 0.0)
+	g.Hero.X = math.Min(g.Hero.X, 620)
+	g.Hero.Y = math.Min(g.Hero.Y, 460)
+}
+
 func (g *Game) Update() error {
-	// if ebiten.IsKeyPressed(ebiten.KeyTab) {
-	// 	g.Paused = !g.Paused
-
-	// }
-
-	// if g.Paused {
-	// 	return nil
-	// }
 	g.Hero.Dx = 0.0
 	g.Hero.Dy = 0.0
-
 	g.caught = false
 
 	if ebiten.IsKeyPressed(ebiten.KeyRight) || ebiten.IsKeyPressed(ebiten.KeyD) {
@@ -96,38 +95,41 @@ func (g *Game) Update() error {
 	g.Hero.Y += g.Hero.Dy
 	CheckCollisionVertical(g.Hero, g.colliders)
 
-	// villain
-	for _, sprite := range g.villain {
-		sprite.Dx = 0.0
-		sprite.Dy = 0.0
+	for _, villain := range g.villains {
+		villain.Dx = 0.0
+		villain.Dy = 0.0
 
-		if sprite.X < g.Hero.X {
-			sprite.Dx += 1
+		xDistance := villain.X - g.Hero.X
+		yDistance := villain.Y - g.Hero.Y
 
-		} else if sprite.X > g.Hero.X {
-			sprite.Dx -= 1
+		speed := 1.0
+
+		if xDistance > 1.0 {
+			villain.Dx -= speed
+		} else if xDistance < -1.0 {
+			villain.Dx += speed
 		}
-		if sprite.Y < g.Hero.Y {
-			sprite.Y += 1
-		} else if sprite.Y > g.Hero.Y {
-			sprite.Y -= 1
-		}
-		sprite.X += sprite.Dx
-		sprite.Y += sprite.Dy
-		CheckCollisionHorizontal(sprite.Sprite, g.colliders)
-		CheckCollisionVertical(sprite.Sprite, g.colliders)
 
-		// caught
-		// if math.Abs(sprite.X-g.Hero.X) < 15 && math.Abs(sprite.Y-g.Hero.Y) < 15 {
-		// 	g.caught = true
-		// }
+		if yDistance > 1.0 {
+			villain.Dy -= speed
+		} else if yDistance < -1.0 {
+			villain.Dy += speed
+		}
+
+		villain.X += villain.Dx
+		CheckCollisionHorizontal(villain.Sprite, g.colliders)
+		villain.Y += villain.Dy
+		CheckCollisionVertical(villain.Sprite, g.colliders)
+
+		if math.Abs(villain.X-g.Hero.X) < 15 && math.Abs(villain.Y-g.Hero.Y) < 15 {
+			g.caught = true
+		}
 	}
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// screen.Fill(color.RGBA{120, 180, 255, 255})
 	screen.Fill(color.White)
 	opts := ebiten.DrawImageOptions{}
 
@@ -138,53 +140,31 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		&opts)
 	opts.GeoM.Reset()
 
-	// renduring multiple villains
-	for _, sprite := range g.villain {
-
-		opts.GeoM.Translate(sprite.X, sprite.Y)
-
-		screen.DrawImage(
-			sprite.img.SubImage(
-				image.Rect(0, 0, 16, 16)).(*ebiten.Image),
-			&opts)
+	for _, villain := range g.villains {
+		opts.GeoM.Translate(villain.X, villain.Y)
+		screen.DrawImage(villain.img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image), &opts)
 		opts.GeoM.Reset()
 	}
-	opts.GeoM.Reset()
-	// border
-	g.Border(g.Hero.X, g.Hero.Y)
+
+	g.Border()
 	if g.caught {
-		ebitenutil.DebugPrint(screen, "you get cought")
+		ebitenutil.DebugPrint(screen, "You got caught!")
 	}
-	// if g.Paused {
-	// 	ebitenutil.DebugPrint(screen, "you have paused")
-	// }
 
 	for _, col := range g.colliders {
-		vector.StrokeRect(screen,
+		vector.DrawFilledRect(screen,
 			float32(col.Min.X),
 			float32(col.Min.Y),
 			float32(col.Dx()),
 			float32(col.Dy()),
-			1.0,
-			color.RGBA{255, 0, 0, 255},
-			true)
-
+			color.RGBA{0, 0, 5, 100},
+			true,
+		)
 	}
-
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return ebiten.WindowSize()
-}
-
-func (g *Game) Border(width, hight float64) {
-	g.Hero.X = math.Max(g.Hero.X, 0.0)
-
-	g.Hero.Y = math.Max(g.Hero.Y, 0.0)
-
-	g.Hero.X = math.Min(g.Hero.X, 620)
-	g.Hero.Y = math.Min(g.Hero.Y, 460)
-
+	return 640, 480
 }
 
 func main() {
@@ -201,44 +181,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	game := Game{
 		Hero: &Sprite{
 			img: hero,
-			X:   50.0,
-			Y:   50.0,
+			X:   300.0,
+			Y:   400.0,
 		},
-		villain: []*Villain{
-
-			{
-				&Sprite{
-					img: villain,
-					X:   175.0,
-					Y:   175.0,
-				},
-			},
-
-			{
-				&Sprite{
-					img: villain,
-					X:   50.0,
-					Y:   50.0,
-				},
-			},
-
-			{
-				&Sprite{
-					img: villain,
-					X:   100.0,
-					Y:   100.0,
-				},
-			},
+		villains: []*Villain{
+			{&Sprite{img: villain, X: 0.0, Y: 0.0}},
+			{&Sprite{img: villain, X: 50.0, Y: 150.0}},
+			{&Sprite{img: villain, X: 500.0, Y: 300.0}},
 		},
 		colliders: []image.Rectangle{
-			image.Rect(100, 180, 116, 116),
-			image.Rect(100, 280, 116, 216),
-			// 	image.Rect(200, 680, 116, 116),
-			// 	image.Rect(700, 380, 116, 116),
-			// 	image.Rect(900, 80, 116, 116),
+			image.Rect(150, 100, 116, 216),
+			image.Rect(100, 80, 200, 60),
+			image.Rect(550, 100, 316, 216),
+			image.Rect(300, 80, 400, 60),
+			image.Rect(750, 100, 516, 716),
+			image.Rect(500, 80, 600, 60),
 		},
 	}
 
